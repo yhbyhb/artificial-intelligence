69c30afa6141f248d9b362a9517f982274363c67
HanByul Yang <hanbyul.yang@gmail.com>
2020-08-10 22:10:22 +0900

    updated result extraction script. added result excel files.

--- Projects/2_Classical Planning/experiments/result2csv.py
+++ Projects/2_Classical Planning/experiments/result2csv.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def result2csv(target):
-    csv_columns = ['P','S','Actions', 'Expansions', 'Goal Tests', 'New Nodes', 'Plan length', 'Time elapsed in seconds']
+    csv_columns = ['P','S', 'Problem', 'Search algorithm', 'Actions', 'Expansions', 'Goal Tests', 'New Nodes', 'Plan length', 'Time elapsed in seconds']
     dict_data = []
 
     files = os.listdir(target)
@@ -12,12 +12,17 @@ def result2csv(target):
         problem, solution = filename.split('_')
 
         dict_item = {}
-        dict_item['P'] = problem
-        dict_item['S'] = solution
+        dict_item['P'] = int(problem.replace('p', ''))
+        dict_item['S'] = int(solution.replace('s', ''))
         with open (os.path.join(target, file), "r") as f:
             data=f.readlines()
+            # print(data)
+            ps = data[1].split('using')
             result_line0 = data[4].split()
             result_line1 = data[6].split()
+
+            dict_item['Problem'] = ps[0].strip().replace('Solving ', '')
+            dict_item['Search algorithm'] = ps[1].strip().replace('...', '')
             dict_item['Actions'] = result_line0[0]
             dict_item['Expansions'] = result_line0[1]
             dict_item['Goal Tests'] = result_line0[2]
